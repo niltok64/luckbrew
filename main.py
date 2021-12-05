41da03cb935b14829b7d549b56e25fab950f9669
DaStickMan <72703236+dastikman@users.noreply.github.com>
2021-12-05 18:00:37 +0000

    finished window part of main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# import all classes of guizero cus i'm lazy to add guizero. behind all the classes
 from guizero import *
+
 app = App(title="Luckbrew")
 logo = Picture(app, image="logo.png")
 welcome = Text(app, text="Welcome to Luckbrew!")
