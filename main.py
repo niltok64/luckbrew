22d3bb7aaa2c0013a69d42c3c3970eabad5970ce
DaStickMan <72703236+dastikman@users.noreply.github.com>
2021-12-06 16:12:20 +0000

    0.0.1

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ pot_o_luck = 0
 luck_nut = 0
 luck_bank = 0
 
+# functions
+def brew():
+    info("Brew", ("Thank you for brewing a " + brew_options.value))
+
+# the actual code
 app = App(title="Luckbrew")
 logo = Picture(app, image="logo.png")
 welcome = Text(app, text="Welcome to Luckbrew!")
+brew_text = Text(app, text="What would you like to brew?")
+brew_options = Combo(app, options=["Potion of Luck", "Pot O\' Luck", "Luck Nut", "Luck Bank"])
+brew_button = PushButton(app, text="Brew", command=brew)
+
 app.display()
