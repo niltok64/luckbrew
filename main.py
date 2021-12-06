cd5449df3366f5d959089e3285370221a459761d
DaStickMan <72703236+dastikman@users.noreply.github.com>
2021-12-06 15:52:46 +0000

    added variables and comments in main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # import all classes of guizero cus i'm lazy to add guizero. behind all the classes
 from guizero import *
 
+# variables
+potion_of_luck = 0
+pot_o_luck = 0
+luck_nut = 0
+luck_bank = 0
+
 app = App(title="Luckbrew")
 logo = Picture(app, image="logo.png")
 welcome = Text(app, text="Welcome to Luckbrew!")
